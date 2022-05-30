398587394e2162db1d4eb892b8cd85c2fa8c3ddc
Shuya Fukushima <shuya102711@gmail.com>
2022-05-30 16:45:21 +0900

    first commit

---
+++ cmd/root.go
@@ -0,0 +1,34 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+)
+
+// todo: 特定の拡張子のみ検出、ファイルの中身まで探索
+var RootCmd = &cobra.Command{
+	Use:   "fingo",
+	Short: "File system walker",
+	Run: func(cmd *cobra.Command, args []string) {
+		jikko(args)
+	},
+}
+
+func jikko(args []string) {
+	if err := filepath.Walk(args[0], crawl); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func crawl(path string, file_info os.FileInfo, err error) error {
+	if file_info.IsDir() {
+		fmt.Printf(" dir : %s\n", path)
+		return nil
+	} else {
+		fmt.Printf("file : %s\n", path)
+	}
+	return nil
+}
